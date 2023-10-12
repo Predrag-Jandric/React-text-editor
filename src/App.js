6412f93a7832329962af54453e03ce3700ef39e3
predrag jandric <predrag.jandric.bg@gmail.com>
2023-10-12 23:49:35 +0200

    added dark / light mode and improved responsivness

--- src/App.js
+++ src/App.js
@@ -1,38 +1,41 @@
 import './App.scss';
-  
+
 //Require Editor CSS files.
 import "froala-editor/css/froala_style.min.css";
 import "froala-editor/css/froala_editor.pkgd.min.css";
 import "froala-editor/js/plugins.pkgd.min.js";
 import FroalaEditorComponent from "react-froala-wysiwyg";
 
-export default function  App() {
-  
+import { useState } from 'react';
 
-  
-   return ( 
-    
-    
-    
-    <div className="app">
+export default function App() {
+  const [isDark, setIsDark] = useState(true)
 
-    <article className='header'>
-      <h1>Text editor with local storage and dark / light mode</h1>
-      <button 
-      onClick={() => alert("NO FUNCTIONALITY TO THIS BTN")}
-      className="editor-container__btn"
-      >Submit</button>
-    </article>
-    
-    <article className="editor-container">
-      <FroalaEditorComponent 
-      tag="textarea"
-      />
-      <button 
-      onClick={() => alert("NO FUNCTIONALITY TO THIS BTN")}
-      className="editor-container__btn"
-      >Submit</button>
+  const toggleTheme = () => {
+    setIsDark(!isDark)
+  }
+
+  return (
+    <section className={`main ${isDark ? 'dark' : 'light'}`}>
+
+      <article className='header'>
+        <h1 className='header__h1'>Text editor with local storage and dark / light mode</h1>
+        <button
+          onClick={toggleTheme}
+          className="globalBtn"
+        >{isDark ? "Light mode" : "Dark mode"}
+        </button>
       </article>
-    </div>
-      );
-    }
+
+      <article className="text-editor">
+        <FroalaEditorComponent
+          tag="textarea"
+        />
+        <button
+          onClick={() => alert("NO FUNCTIONALITY TO THIS BTN")}
+          className="globalBtn"
+        >Submit</button>
+      </article>
+    </section>
+  );
+}
